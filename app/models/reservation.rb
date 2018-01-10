48b1501b896d9bd62fe2dd42bf3bc373bb5beded
nongp <nongp@hotmail.com>
2018-01-10 19:11:59 +0700

    make it better

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -15,9 +15,9 @@ class Reservation < ApplicationRecord
   private
 
     def create_notification
-      type = self.room.Instant? ? "New Booking" : "New Request"
+      type = self.room.Instant? ? "การจอง!" : "มี Request การจอง!"
       guest = User.find(self.user_id)
 
-      Notification.create(content: "#{type} from #{guest.fullname}", user_id: self.room.user_id)
+      Notification.create(content: "#{type} จาก #{guest.fullname}", user_id: self.room.user_id)
     end
 end
