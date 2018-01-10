e9cc5a6ebf957d7a67ab257b7b78fc4c190fb57d
nongp <nongp@hotmail.com>
2018-01-10 19:44:58 +0700

    make it better

--- db/migrate/20170705060049_add_unread_to_users.rb
+++ db/migrate/20170705060049_add_unread_to_users.rb
@@ -1,5 +1,5 @@
 class AddUnreadToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :unread, :boolean, default: true
+    add_column :users, :unread, :integer, default: 0
   end
 end
