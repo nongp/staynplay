c30f3c09b3c13733b8f2b7e1922d220e50620e8e
nongp <nongp@hotmail.com>
2018-01-20 08:54:07 +0700

    commit for deploy to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
    config.action_cable.mount_path = nil
    config.action_cable.url = 'ws://immense-beyond-70357.herokuapp.com/cable'
-   config.action_cable.allowed_request_origins = [ 'https://immense-beyond-70357.herokuapp.com', /https:\/\/staynplay.co*/ ]
+  # config.action_cable.allowed_request_origins = [ 'https://immense-beyond-70357.herokuapp.com', /https:\/\/staynplay.co*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
    config.force_ssl = true
